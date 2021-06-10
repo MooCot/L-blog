03a126e62192d7deb607c7967a8c31876f00826c
morrowind <gitmooncat@gmail.com>
2021-06-10 23:24:23 +0300

    styme main view admin

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -3,11 +3,18 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\Post;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    public function index(){
-			return view('admin.home.index');
+    public function index()
+		{
+
+				$post_count = Post::all()->count();
+
+				return view('admin.home.index', [
+					'posts_count' => $post_count
+				]);
 		}
 }
