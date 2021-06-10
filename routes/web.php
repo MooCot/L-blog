915b04a22157da7c80632bf78edf7d7195960945
morrowind <gitmooncat@gmail.com>
2021-06-10 19:12:24 +0300

    create autorisation system

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,17 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+
+Route::group(['middleware' => ['role:admin']], function () {
+
+	Route::get('/test', function () {
+		return view('test');
+	});
+
+});
+
