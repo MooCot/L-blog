72f280d373be09eaf679e9f4d644bbcf9126bcc1
morrowind <gitmooncat@gmail.com>
2021-06-10 22:33:51 +0300

    create admin view

--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,7 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
+ */
 
 Route::get('/', function () {
     return view('welcome');
@@ -21,12 +21,8 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
+Route::middleware(['role:admin'])->prefix('admin_panel')->group(function () {
 
-Route::group(['middleware' => ['role:admin']], function () {
-
-	Route::get('/test', function () {
-		return view('test');
-	});
+	Route::get('/',  [App\Http\Controllers\Admin\HomeController::class, 'index']);
 
 });
-
